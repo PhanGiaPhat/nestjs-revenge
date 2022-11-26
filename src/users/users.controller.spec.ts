817ea5ef0ba299e532da33ff55f6a4d79f329353
PhanGiaPhat <phat@oven.bz>
2022-11-26 16:22:18 +0700

    finish user controller unit test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -8,6 +8,20 @@ describe('Users Controller', () => {
   let usersService: UsersService;
 
   const mockUserId = '1';
+  const mockUserArrs = [
+    {
+      id: '1',
+      name: 'name 1',
+    },
+    {
+      id: '2',
+      name: 'name 2',
+    },
+  ];
+  const mockUser = {
+    id: 'id',
+    name: 'name',
+  };
 
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -19,7 +33,12 @@ describe('Users Controller', () => {
             create: jest.fn().mockResolvedValueOnce({
               id: mockUserId,
             }),
-            findAll: jest.fn(),
+            findAll: jest.fn().mockResolvedValueOnce(mockUserArrs),
+            findOne: jest.fn().mockResolvedValueOnce(mockUser),
+            update: jest.fn().mockResolvedValueOnce({
+              id: mockUserId,
+            }),
+            remove: jest.fn(),
           },
         },
       ],
@@ -31,7 +50,7 @@ describe('Users Controller', () => {
 
   afterEach(jest.clearAllMocks);
 
-  it.only('create user function should work well', async () => {
+  it('create user function should work well', async () => {
     const mockCreateUserDto: CreateUserDto = {
       name: 'test 1',
     };
@@ -46,8 +65,48 @@ describe('Users Controller', () => {
       id: mockUserId,
     });
   });
-  it('findAll user function should work well', async () => {});
-  it('findOne user function should work well', async () => {});
-  it('update user function should work well', async () => {});
-  it('remove user function should work well', async () => {});
+
+  it('findAll user function should work well', async () => {
+    const result = await usersController.findAll();
+
+    expect(usersService.findAll).toBeCalledTimes(1);
+
+    expect(result).toEqual(mockUserArrs);
+  });
+
+  it('findOne user function should work well', async () => {
+    const id = '1';
+
+    const result = await usersController.findOne(id);
+
+    expect(usersService.findOne).toBeCalledTimes(1);
+
+    expect(usersService.findOne).toBeCalledWith(id);
+
+    expect(result).toEqual(mockUser);
+  });
+  it('update user function should work well', async () => {
+    const id = '1';
+
+    const userUpdate: CreateUserDto = {
+      name: 'name',
+    };
+
+    const result = await usersController.update(id, userUpdate);
+
+    expect(usersService.update).toBeCalledTimes(1);
+
+    expect(usersService.update).toBeCalledWith(id, userUpdate);
+
+    expect(result).toEqual({
+      id: mockUserId,
+    });
+  });
+  it('remove user function should work well', async () => {
+    const id = '1';
+
+    await usersController.remove(id);
+
+    expect(usersService.remove).toBeCalledTimes(1);
+  });
 });
